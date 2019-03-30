8f8c598030e5ba18295e063f113079055997df48
youngMen1 <fengzhiqiangzxx199644>
2019-03-30 18:14:53 +0800

    Initial commit

---
+++ permission/src/main/java/com/gdfl/permission/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.gdfl.permission.controller;
+
+/**
+ * @author zhiqiang.feng
+ * @version 1.0
+ * @date-time 2019/3/30 17:59
+ * @description https://www.cnblogs.com/lhyqzx/p/5962826.html
+ * https://www.cnblogs.com/zhexiu/p/7816055.html
+ * https://blog.csdn.net/u011277123/article/details/68940939
+ **/
+public class UserController {
+}
